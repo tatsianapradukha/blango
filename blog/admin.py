af748c9f6ea289ffeb3c5a08f58ee0fb48638cf8
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-08-13 15:53:13 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
-admin.site.register(Tag)
+
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display =("slug", "published_at")
+    #list_display =("slug", "published_at")
 
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Tag)
+admin.site.register(Comment)
