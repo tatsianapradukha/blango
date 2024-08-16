7aa00b9bc6a6c19cb91cc8a4450e0618ea2d045f
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-08-16 13:13:37 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -40,6 +40,28 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+    LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
     # Application definition
 
     INSTALLED_APPS = [
