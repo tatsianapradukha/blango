765e7507f32f0fbd6ed2ac9aa44f2f3604c9ee00
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-08-27 12:49:48 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     RegistrationView.as_view(form_class=BlangoRegistrationForm),
     name="django_registration_register",),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
