40849d9cfa39129d3859f55c4db666630f522456
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-08-28 16:00:06 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     RegistrationView.as_view(form_class=BlangoRegistrationForm),
     name="django_registration_register",),
     path("accounts/", include("django_registration.backends.activation.urls")),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 ]
 
 if settings.DEBUG:
