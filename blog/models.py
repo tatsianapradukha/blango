6b5e062e5e4934036d9249d29b253486eaf4f99f
TATSIANA PRADUKHA <tatyanaproduho@mail.ru>
2024-09-16 13:54:21 +0000

    Finish pagination and django filter

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,9 @@ class Tag(models.Model):
     value = models.TextField(max_length=100, unique=True)
     def __str__(self):
         return self.value
+    class Meta:
+        ordering = ["value"]
+
 
 class Comment(models.Model):
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
